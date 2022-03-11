bf14e8a6a673d2f670e33a06be7fc1ba19a5d70e
larizzatg <larizzatg@hotmail.com>
2022-03-11 14:35:38 -0400

    feat: use db repository for saving metrics on service

--- apps/api/src/metrics/metrics.service.ts
+++ apps/api/src/metrics/metrics.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { CreateMetricDto } from './dto/create-metric.dto'
 import { Metric } from './entities/metric.entity'
-
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
 @Injectable()
 export class MetricsService {
-  private metrics: Metric[] = []
+  constructor(
+    @InjectRepository(Metric)
+    private readonly metricRepository: Repository<Metric>,
+  ) {}
 
   getAllMetrics() {
-    return this.metrics
+    return this.metricRepository.find()
   }
 
   createMetric(createMetricDto: CreateMetricDto) {
-    const { name, value } = createMetricDto
-    const metric: Metric = {
-      name,
-      value,
-      timestamp: new Date(),
-    }
-
-    this.metrics.push(metric)
+    const metric = this.metricRepository.create(createMetricDto)
+    return this.metricRepository.save(metric)
   }
 }
