43eb7d0fe7b99117f7e3258d798b250ad0f93ed0
larizzatg <larizzatg@hotmail.com>
2022-03-12 19:11:14 -0400

    feat: add input component

---
+++ apps/web/src/components/app-input/app-input.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface InputProps {
+  type?: string
+  placeholder?: string
+  modelValue?: string
+}
+defineProps<InputProps>()
+const emit = defineEmits(['update:modelValue'])
+
+const onInput = (payload: Event) => {
+  const target = payload.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <input
+    :type="type || 'text'"
+    :value="modelValue"
+    @input="onInput"
+    class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 border-2 border-indigo-100 rounded-lg appearance-none focus:outline-none focus:border-indigo-400 bg-gray-50"
+  />
+</template>
