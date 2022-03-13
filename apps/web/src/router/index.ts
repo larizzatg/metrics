c94fa658fce5ab5c53ce7f95c516b2a858538576
larizzatg <larizzatg@hotmail.com>
2022-03-13 19:17:02 -0400

    refactor: rename home to timeline

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Timeline from '../views/Timeline.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'timeline',
+      component: Timeline,
     },
   ],
 })
