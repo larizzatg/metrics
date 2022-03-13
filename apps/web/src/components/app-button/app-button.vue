ea480bd77c56ca2f6eac5614700264c998298a2a
larizzatg <larizzatg@hotmail.com>
2022-03-13 08:52:14 -0400

    fix: change primary color to bg-blue-500

--- apps/web/src/components/app-button/app-button.vue
+++ apps/web/src/components/app-button/app-button.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="flex py-3 text-base font-medium leading-5 text-white bg-blue-600 rounded px-9 focus:outline-none focus:ring-2 hover:bg-blue-700"
+    class="flex py-3 text-base font-medium leading-5 text-white bg-blue-500 rounded px-9 focus:outline-none focus:ring-2 hover:bg-blue-600"
   >
     <slot />
   </button>
