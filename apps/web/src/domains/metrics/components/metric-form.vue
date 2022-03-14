784bfdd69a7b47b6940c609600d518b0f5896e01
larizzatg <larizzatg@hotmail.com>
2022-03-13 23:23:30 -0400

    feat: add metric form with simple validation

--- apps/web/src/domains/metrics/components/metric-form.vue
+++ apps/web/src/domains/metrics/components/metric-form.vue
@@ -5,22 +5,57 @@ import AppInput from '@/components/app-input/app-input.vue'
 import AppButton from '@/components/app-button/app-button.vue'
 import { reactive } from 'vue'
 
-const form = reactive({
+interface MetricForm {
+  name: string
+  value: number
+}
+type FormErrors<T> = Record<keyof T, string[]>
+
+const form = reactive<MetricForm>({
   name: '',
   value: 0,
 })
+const formErrors = reactive<FormErrors<MetricForm>>({
+  name: [],
+  value: [],
+})
+
+const emit = defineEmits(['submit'])
+
+const validateForm = () => {
+  let valid = true
+  Object.entries(form).forEach(([key, value]) => {
+    if (value !== 0 && !value) {
+      formErrors[key as keyof MetricForm].push(
+        `The ${key} is necessary for the metric form`,
+      )
+      valid = false
+    } else {
+      formErrors[key as keyof MetricForm] = []
+    }
+  })
+  return valid
+}
+const onSubmit = () => {
+  if (validateForm()) {
+    emit('submit', form)
+  }
+}
 </script>
 
 <template>
-  <app-card title="Create new metric">
-    <form>
-      <app-field id="metric-name" label="name">
-        <app-input v-model="form.name" />
-      </app-field>
-      <app-field id="metric-value" label="value" class="mt-5">
-        <app-input v-model="form.value" />
-      </app-field>
-      <app-button class="mt-6">Add Metric</app-button>
-    </form>
-  </app-card>
+  <form @submit.prevent="onSubmit">
+    <app-field id="metric-name" label="name" :errors="formErrors.name">
+      <app-input v-model="form.name" />
+    </app-field>
+    <app-field
+      id="metric-value"
+      label="value"
+      class="mt-5"
+      :errors="formErrors.value"
+    >
+      <app-input type="number" v-model="form.value" />
+    </app-field>
+    <app-button class="mt-6">Add Metric</app-button>
+  </form>
 </template>
--- apps/web/src/domains/metrics/views/MetricsDashboard.vue
+++ apps/web/src/domains/metrics/views/MetricsDashboard.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import AppButton from '@/components/app-button/app-button.vue'
 import AppCard from '@/components/app-card/app-card.vue'
+import AppModal from '@/components/app-modal/app-modal.vue'
+import MetricForm from '../components/metric-form.vue'
+
+const showMetricFormModal = ref(false)
+const createMetric = (payload) => {
+  console.log('create')
+  console.log(payload)
+}
 </script>
 
 <template>
   <main>
+    <app-modal title="add new metric" v-model:open="showMetricFormModal">
+      <metric-form @submit="createMetric" />
+    </app-modal>
     <div class="flex justify-end flex-1 my-4">
-      <app-button type="secondary"> Create new Metric </app-button>
+      <app-button type="secondary" @click="showMetricFormModal = true">
+        New metric
+      </app-button>
     </div>
     <app-card title="Daily Metrics">
       <template #before> </template>
