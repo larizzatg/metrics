4db147afb2d325434504f8c254992d7f7c4aef5c
larizzatg <larizzatg@hotmail.com>
2022-03-14 10:16:09 -0400

    fix: allow option title on app-card

--- apps/web/src/components/app-card/app-card.vue
+++ apps/web/src/components/app-card/app-card.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps<{
-  title: string
+  title?: string
   squarePadding?: boolean
 }>()
 </script>
@@ -9,7 +9,7 @@ defineProps<{
   <div class="bg-white" :class="squarePadding ? 'px-4 py-4' : 'px-4 py-8'">
     <header class="flex flex-wrap items-center justify-center mb-4">
       <slot name="header">
-        <h5 class="flex-1 caption">{{ title }}</h5>
+        <h5 class="flex-1 caption" v-if="title">{{ title }}</h5>
         <slot name="menu" />
       </slot>
     </header>
