92648e4d21d56a6632e94c01a8eefd86e707b2f9
larizzatg <larizzatg@hotmail.com>
2022-03-13 22:47:05 -0400

    feat: add squared padding on cards

--- apps/web/src/components/app-card/app-card.stories.ts
+++ apps/web/src/components/app-card/app-card.stories.ts
@@ -46,7 +46,14 @@ export const WithCustomHeader = () => ({
         <template #header>
           <h3 class="mb-5 text-center">Love lollipops</h3> 
         </template>
-        <p>Lollipop jujubes chupa chups halvah candy canes. Jelly-o chocolate cake danish biscuit gummies sesame snaps. Muffin cookie carrot cake chupa chups dragée caramels halvah. Pie chupa chups apple pie jujubes lollipop bear claw gummies pudding. Chocolate sugar plum jelly sugar plum lemon drops jelly beans topping. Pudding pie bear claw ice cream chocolate jujubes gummies sweet topping. </p>
+        <p>
+          Lollipop jujubes chupa chups halvah candy canes. 
+          Jelly-o chocolate cake danish biscuit gummies sesame snaps. 
+          Muffin cookie carrot cake chupa chups dragée caramels halvah.
+          Pie chupa chups apple pie jujubes lollipop bear claw gummies pudding. 
+          Chocolate sugar plum jelly sugar plum lemon drops jelly beans topping. 
+          Pudding pie bear claw ice cream chocolate jujubes gummies sweet topping. 
+        </p>
       </app-card>
     </div>`,
 })
--- apps/web/src/components/app-card/app-card.vue
+++ apps/web/src/components/app-card/app-card.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 defineProps<{
   title: string
+  squarePadding?: boolean
 }>()
 </script>
 
 <template>
-  <div class="px-4 py-8 bg-white">
-    <header class="flex items-center justify-center mb-5">
+  <div class="bg-white" :class="squarePadding ? 'px-4 py-4' : 'px-4 py-8'">
+    <header class="flex flex-wrap items-center justify-center mb-4">
       <slot name="header">
         <h5 class="flex-1 caption">{{ title }}</h5>
         <slot name="menu" />
