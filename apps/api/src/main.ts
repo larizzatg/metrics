6238d627bb75063e0ceebcf40579ea967224cff9
larizzatg <larizzatg@hotmail.com>
2022-03-14 14:54:41 -0400

    fix: enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,26 @@
-import { ValidationPipe } from '@nestjs/common'
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
+      validationError: { target: false, value: false },
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new BadRequestException(errors)
+      },
     }),
   )
   await app.listen(4000)
