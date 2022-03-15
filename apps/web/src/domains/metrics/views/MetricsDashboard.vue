a25986b1886a1dc452e70c1547c05c4229dd23d7
larizzatg <larizzatg@hotmail.com>
2022-03-15 16:43:04 -0400

    refactor: use timeline composable and timeline chart

--- apps/web/src/domains/metrics/views/MetricsDashboard.vue
+++ apps/web/src/domains/metrics/views/MetricsDashboard.vue
@@ -1,47 +1,14 @@
 <script setup lang="ts">
-import { computed, reactive, ref, toRaw, watchEffect } from 'vue'
+import { ref } from 'vue'
 import AppButton from '@/components/app-button/app-button.vue'
-import AppCard from '@/components/app-card/app-card.vue'
 import AppModal from '@/components/app-modal/app-modal.vue'
 import MetricForm from '../components/metric-form.vue'
-import MetricChartTimeline from '../timeline/timeline-chart.vue'
-import { getMetricAverages } from '../api'
-import type { MetricAverage, MetricTimelineFilters } from '../types'
-import { useToast } from 'vue-toastification'
+import TimelineChart from '../timeline/timeline-chart.vue'
+import { useTimeline } from '../timeline/useTimeline'
 
-const toast = useToast()
 const showMetricFormModal = ref(false)
-
-const loading = ref(false)
-const timeline = ref<MetricAverage[]>([])
-const timelineFilters = reactive<MetricTimelineFilters>({
-  name: 'performance',
-  interval: 'day',
-  startDate: '',
-  endDate: '',
-})
-
-const filtersToApply = computed(() => {
-  const filters = Object.fromEntries(
-    Object.entries(timelineFilters).filter(([key, value]) => {
-      return !!value
-    }),
-  )
-  return filters as unknown as MetricTimelineFilters
-})
-
-watchEffect(async () => {
-  loading.value = true
-
-  const { data, error } = await getMetricAverages(filtersToApply.value)
-  loading.value = false
-  if (error) {
-    toast.error('There was a problem filtering the data')
-    return
-  }
-
-  timeline.value = data ?? []
-})
+const { filters, loading, data, firstError } = useTimeline()
+filters.value.name = ''
 </script>
 
 <template>
@@ -54,9 +21,6 @@ watchEffect(async () => {
         New metric
       </app-button>
     </div>
-    <app-card title="Daily Metrics">
-      <span v-if="loading">loading</span>
-      <metric-chart-timeline :metrics="timeline" v-else-if="timeline?.length" />
-    </app-card>
+    <timeline-chart :metrics="data" :loading="loading" :error="firstError" />
   </main>
 </template>
