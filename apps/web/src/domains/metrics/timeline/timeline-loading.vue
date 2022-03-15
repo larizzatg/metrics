e193f8831477a9fd06cb21be3a1c1186befa6772
larizzatg <larizzatg@hotmail.com>
2022-03-15 14:45:32 -0400

    feat: add timeline loading state

---
+++ apps/web/src/domains/metrics/timeline/timeline-loading.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import AppLoader from '@/components/app-loader/app-loader.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center py-10">
+    <app-loader class="-mt-2" />
+    <small class="mt-2">loading the metrics</small>
+  </div>
+</template>
