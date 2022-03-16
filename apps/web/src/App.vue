625cd7aed9253c00e53e1685c5a150bd7c11b217
larizzatg <larizzatg@hotmail.com>
2022-03-16 15:37:37 -0400

    refactor: remove the use of vue-router

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import AppHeader from './components/app-header/app-header.vue'
+import MetricsDashboard from './domains/metrics/views/MetricsDashboard.vue'
 </script>
 
 <template>
   <AppHeader title="Metrics App" />
-  <RouterView class="mx-auto mt-8 md:mt-14 max-w-screen-2xl" />
+  <div class="mx-auto mt-8 md:mt-14 max-w-screen-2xl">
+    <MetricsDashboard />
+  </div>
 </template>
 
 <style lang="postcss">
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,12 +3,10 @@ import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 import App from './App.vue'
-import router from './router'
 
 import './assets/index.css'
 
 const app = createApp(App)
 
-app.use(router)
 app.use(Toast)
 app.mount('#app')
