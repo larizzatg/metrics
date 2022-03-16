333262a148bd782d385a9a560017a9c683c226c7
larizzatg <larizzatg@hotmail.com>
2022-03-16 02:58:20 -0400

    feat: add app-card subtitle

--- apps/web/src/components/app-card/app-card.vue
+++ apps/web/src/components/app-card/app-card.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 defineProps<{
   title?: string
+  subtitle?: string
   squarePadding?: boolean
 }>()
 </script>
@@ -8,10 +9,15 @@ defineProps<{
 <template>
   <div class="bg-white" :class="squarePadding ? 'px-4 py-4' : 'px-4 py-8'">
     <header class="flex flex-wrap items-center justify-center mb-4">
-      <slot name="header">
-        <h5 class="flex-1 caption" v-if="title">{{ title }}</h5>
-        <slot name="menu" />
-      </slot>
+      <div class="flex flex-1">
+        <slot name="header">
+          <h5 class="flex-1 caption" v-if="title">{{ title }}</h5>
+          <slot name="menu" />
+        </slot>
+      </div>
+      <small class="w-full text-sm font-light text-gray-400" v-if="subtitle">
+        {{ subtitle }}
+      </small>
     </header>
     <div>
       <slot></slot>
