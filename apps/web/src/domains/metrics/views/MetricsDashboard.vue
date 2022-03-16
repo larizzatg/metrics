40caf3a68a96c069c8b2b292c30723d000bea935
larizzatg <larizzatg@hotmail.com>
2022-03-16 02:24:51 -0400

    fix: metrics dashboard interval

--- apps/web/src/domains/metrics/views/MetricsDashboard.vue
+++ apps/web/src/domains/metrics/views/MetricsDashboard.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, toRaw } from 'vue'
 import AppButton from '@/components/app-button/app-button.vue'
 import AppModal from '@/components/app-modal/app-modal.vue'
 import MetricForm from '../components/metric-form.vue'
 import TimelineChart from '../timeline/timeline-chart.vue'
 import { useTimeline } from '../timeline/useTimeline'
+import type { MetricTimelineFilters } from '../types'
 
 const showMetricFormModal = ref(false)
 const { filters, loading, data, firstError } = useTimeline()
-filters.value.name = ''
+
+const updateFilters = (f: MetricTimelineFilters): void => {
+  filters.value = { ...toRaw(filters.value), ...f }
+}
 </script>
 
 <template>
@@ -21,6 +25,12 @@ filters.value.name = ''
         New metric
       </app-button>
     </div>
-    <timeline-chart :metrics="data" :loading="loading" :error="firstError" />
+    <timeline-chart
+      :metrics="data"
+      :loading="loading"
+      :error="firstError"
+      :interval="filters.interval"
+      @change="updateFilters"
+    />
   </main>
 </template>
